dfec2ae3229a6a1370448733035698b7a0fbe720
skahler-pivotal <skahler@pivotal.io>
2019-01-08 07:36:23 -0600

    update to bento debian9 image and removal of shared local folder

--- src/tools/vagrant/debian/vagrant-build.sh
+++ src/tools/vagrant/debian/vagrant-build.sh
@@ -1,26 +1,74 @@
-cd /gpdb
+#!/bin/bash
 
+set -ex
+
+# clone the repos
+rm -fr gpos
+rm -fr gp-xerces
+rm -fr gporca
+rm -fr gpdb
+
+pushd ~
+  git clone https://github.com/greenplum-db/gpos
+  git clone https://github.com/greenplum-db/gp-xerces
+  git clone https://github.com/greenplum-db/gporca
+  git clone https://github.com/greenplum-db/gpdb
+popd
+
+export CC="ccache cc"
+export CXX="ccache c++"
+export PATH=/usr/local/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+
+pushd ~/gpos
+  rm -fr build
+  mkdir build
+  pushd build
+    cmake ../
+    make -j4 && sudo make install
+  popd
+popd
+
+pushd ~/gp-xerces
+  rm -fr build
+  mkdir build
+  pushd build
+    ../configure --prefix=/usr/local
+    make -j4 && sudo make install
+  popd
+popd
+
+pushd ~/gporca
+  rm -fr build
+  mkdir build
+  pushd build
+    cmake ../
+    make -j4 && sudo make install
+  popd
+popd
+
+rm -rf /usr/local/gpdb
 sudo mkdir /usr/local/gpdb
 sudo chown vagrant:vagrant /usr/local/gpdb
-./configure --enable-debug --with-python --with-perl --enable-mapreduce --with-libxml --prefix=/usr/local/gpdb
 
-make clean
-make -j 4
-make install
+pushd ~/gpdb
+  ./configure --prefix=/usr/local/gpdb CFLAGS="-I/usr/local/include/ -L/usr/local/lib/" $@
+  make clean
+  make -j4 -s && sudo make install
+popd
 
-cd /gpdb/gpAux
-cp -rp gpdemo /home/vagrant/
-sudo chown -R vagrant:vagrant /home/vagrant/gpdemo
+sudo chown -R vagrant:vagrant /usr/local/gpdb
+
 cat /home/vagrant/.ssh/id_rsa.pub >> /home/vagrant/.ssh/authorized_keys
 # make sure ssh is not stuck asking if the host is known
 ssh-keyscan -H localhost >> /home/vagrant/.ssh/known_hosts
 ssh-keyscan -H 127.0.0.1 >> /home/vagrant/.ssh/known_hosts
-ssh-keyscan -H debian-jessie >> /home/vagrant/.ssh/known_hosts
-cd /home/vagrant/gpdemo
-source /usr/local/gpdb/greenplum_path.sh
-make cluster
 
+# BUG: fix the LD_LIBRARY_PATH to find installed GPOPT libraries
+echo export LD_LIBRARY_PATH=/usr/local/lib:\$LD_LIBRARY_PATH >> /usr/local/gpdb/greenplum_path.sh
 
-#cd /vagrant/src/pl/plspython/tests
-#make containers
-#sudo -u postgres make
+# use gpdemo to start the cluster
+pushd ~/gpdb/gpAux/gpdemo
+  source /usr/local/gpdb/greenplum_path.sh
+  make
+popd
--- src/tools/vagrant/debian/vagrant-setup.sh
+++ src/tools/vagrant/debian/vagrant-setup.sh
@@ -1,31 +1,38 @@
 sudo apt-get update
-sudo apt-get -y install apt-transport-https
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
-sudo bash -c 'echo "deb https://apt.dockerproject.org/repo debian-jessie main" > /etc/apt/sources.list.d/docker.list'
-sudo apt-get update
 sudo apt-get install wget
-sudo apt-get -y install docker-engine build-essential libreadline6 \
-libreadline6-dev zlib1g-dev bison flex git-core libcurl4-openssl-dev \
-python-dev libxml2-dev pkg-config vim libbz2-dev  python-pip \
-libapr1-dev libevent-dev libyaml-dev libperl-dev libffi-dev \
-python-setuptools-whl libssl-dev
+sudo apt-get -y install \
+  bison \
+  build-essential \
+  clang-4.0 \
+  ccache \
+  cmake \
+  flex \
+  git-core \
+  libapr1-dev \
+  libbz2-dev  \
+  libcurl4-openssl-dev \
+  libevent-dev \
+  libffi-dev \
+  libperl-dev \
+  libreadline-dev \
+  libssl-dev \
+  libxml2-dev \
+  libyaml-dev \
+  pkg-config \
+  python-dev \
+  python-pip \
+  vim \
+  zlib1g-dev 
 
 echo locales locales/locales_to_be_generated multiselect     de_DE ISO-8859-1, de_DE ISO-8859-15, de_DE.UTF-8 UTF-8, de_DE@euro ISO-8859-15, en_GB ISO-8859-1, en_GB ISO-8859-15, en_GB.ISO-8859-15 ISO-8859-15, en_GB.UTF-8 UTF-8, en_US ISO-8859-1, en_US ISO-8859-15, en_US.ISO-8859-15 ISO-8859-15, en_US.UTF-8 UTF-8 | debconf-set-selections
 echo locales locales/default_environment_locale      select  en_US.UTF-8 | debconf-set-selections
 dpkg-reconfigure locales -f noninteractive
 
-
 su vagrant -c "ssh-keygen -t rsa -f .ssh/id_rsa -q -N ''"
 
-pip install --upgrade pip
-pip install cffi --upgrade
-pip install lockfile
-pip install paramiko --upgrade
-pip install setuptools --upgrade
-pip install --pre psutil
-pip install cryptography --upgrade
-
-sudo service docker start
-sudo useradd postgres
-sudo usermod -aG docker postgres
-sudo usermod -aG docker vagrant
+sudo pip install --upgrade psutil
+sudo pip install --upgrade lockfile
+sudo pip install --upgrade paramiko
+sudo pip install --upgrade setuptools
+sudo pip install --upgrade epydoc
+sudo pip install --upgrade pyyaml
